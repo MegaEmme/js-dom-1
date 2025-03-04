074077333aa885b23597297cea5e621cbb4c967f
MegaEmme <mikmusco@gmail.com>
2025-03-04 15:28:57 +0100

    feat. added first cycle

--- bonus/script.js
+++ bonus/script.js
@@ -1,9 +1,18 @@
-console.log('Mike bonus');
+console.log("Mike's bonus");
 
 const lamp = document.getElementById('bulb');
 console.log(lamp);
+
 const pressHere = document.getElementById('button');
 console.log(pressHere);
 pressHere.addEventListener('click',function(){
     lamp.src="../img/yellow_lamp.png";
+    pressHere.innerText = 'Spegni';
+    pressHere.addEventListener('click',function(){
+        lamp.src="../img/white_lamp.png";
+        pressHere.innerText = 'Accendi';
+    })
 })
+
+
+
