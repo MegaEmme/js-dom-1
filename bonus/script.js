0dd739dd3a03a0dced3322cd61d94771f4edfa45
MegaEmme <mikmusco@gmail.com>
2025-03-04 14:32:23 +0100

    feat: create bonus folder

---
+++ bonus/script.js
@@ -0,0 +1,9 @@
+console.log('Mike');
+
+const lamp = document.getElementById('bulb');
+console.log(lamp);
+const pressHere = document.getElementById('button');
+console.log(pressHere);
+pressHere.addEventListener('click',function(){
+    lamp.src="../img/yellow_lamp.png";
+})
